e30c7530b5b6318e4f4d95d0acefb372a5796ac6
Luís Felipe Borges Silva <luis.felipe@cosmetologia.local>
2021-01-04 13:54:22 -0300

    objeto

---
+++ fundamentos/11. objeto.js
@@ -0,0 +1,20 @@
+const prod1 = {}
+
+prod1.nome = 'Celular ultra mega'
+prod1.preco = 4998.90
+
+prod1['desconto zika'] = 0.40 // nome com espaço, mas evitar
+
+console.log (prod1)
+
+
+const prod2 = {
+
+    nome: 'camisa polo',
+    preco: 79.90,
+
+    obj: {
+        blabla:1
+    }
+
+}
