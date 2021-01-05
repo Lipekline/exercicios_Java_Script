9cc0d652af6ff0058168e3ad8724323911f8a0e5
Luis Felipe Borges Silva <58269629+Lipekline@users.noreply.github.com>
2021-01-05 18:35:56 -0300

    teste2

--- fundamentos/14. funcao1.js
+++ fundamentos/14. funcao1.js
@@ -17,5 +17,5 @@ function soma (a, b = 0){
 
 soma(2, 3) // não exibe nada pois a função apenas retorna a soma
 console.log(soma(2, 3))
-
+console.log(soma(2)) // como o valor de b foi tratado na criação da função o retorno não será NaN
 
